a0a35f97bb0b01dc45d32718b44dde0cc16f6329
natagetup <nata.getup@gmail.com>
2021-02-24 14:07:49 +0300

    feat: add solution dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+if (Array.isArray(members)) {
+    let firstLetters = '';
+
+    members.forEach(function(item, index, array) {
+        if (typeof item === "string") {
+          firstLetters += item.trim().charAt(0);
+        }
+    });
+    
+    let result = firstLetters.toUpperCase().split('').sort().join('');
+    return result;
+} else {
+    return false
+}
+
 };
