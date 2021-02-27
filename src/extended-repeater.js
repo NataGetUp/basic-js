62e202eeec6a6b0dd9ec3e5a4e66cb219dde9136
natagetup <nata.getup@gmail.com>
2021-02-27 22:11:26 +0300

    fix: сorrect mistakes in task extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,14 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(str, options) {
+module.exports = function repeater(str, { repeatTimes = 1, separator = '+', addition = '', additionRepeatTimes = 1, additionSeparator = '|' }) {
   let additStr = '';
   let mainStr = '';
 
   let mainArr = [];
   let additArr = [];
 
-  let timeMain = options.repeatTimes;
-  let timeAddit = options.additionRepeatTimes;
+  let timeMain = repeatTimes;
+  let timeAddit = additionRepeatTimes;
 
   if(typeof timeMain == "undefined") {
     timeMain = 1;
@@ -19,10 +19,10 @@ module.exports = function repeater(str, options) {
   }
 
   for(let i = 1; i <= timeAddit; i++) {
-    additArr.push(options.addition);
+    additArr.push(String(addition));
   }
 
-  additStr = additArr.join(options.additionSeparator || '|');
+  additStr = additArr.join(String(additionSeparator) || '|');
 
 
   for(let i = 1; i <= timeMain; i++) {
@@ -31,7 +31,7 @@ module.exports = function repeater(str, options) {
   }
 
 
-  mainStr = mainArr.join(options.separator || '+');
+  mainStr = mainArr.join(String(separator) || '+');
 
   return mainStr;
 
